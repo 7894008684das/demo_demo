0426c23b1e203e5aff1c90105d734aac5a738344
Abhishek das <7894008684das@gmail.com>
2022-02-16 16:33:06 +0530

    jhfkhfks

---
+++ New folder/test.py
@@ -0,0 +1,31 @@
+x = 1.6
+list
+
+x = [1,2,"ghfgsdh",,,,,,,,]
+dict/json
+x = {happy:[],"ghgsda":"sgsg",}
+
+tuple
+x = (1,2,3,"safgsfg",'sgfsgsg')
+
+boolean
+True
+False
+1
+0
+
+
+
+
+
+
+
+
+y = 10//3
+
+print(y)
+
+
+
+
+
